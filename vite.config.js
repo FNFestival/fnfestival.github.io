025d04dfb904e2f8c9e0cf677c1de1aa49710313
FNFestival <github@fnfestival.co>
2025-10-29 22:12:29 +0100

    Fix syntax error in vite.config.js by correcting missing commas

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
       closeBundle() {
         copyFileSync('CNAME', 'dist/CNAME');
         cpSync('data', 'dist/data', { recursive: true });
-      }
-    }
-  ]
+      },
+    },
+  ],
 });
